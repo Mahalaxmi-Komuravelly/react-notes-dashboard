15fa629f05f21d4f1f06632356a6eed6f9292f70
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2026-03-08 17:00:30 +0530

    feat:Add Notes Count Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NoteInput from './components/NoteInput'
 import NotesList from './components/NotesList'
+import NotesCount from './components/NotesCount'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <h1>Notes Dashboard</h1>
       <NoteInput/>
       <NotesList/>
+      <NotesCount/>
     </div>
   )
 }
--- src/components/NotesCount.jsx
+++ src/components/NotesCount.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useNotes } from '../context/NotesContext'
 
 const NotesCount = () => {
+    const {notes} = useNotes();
+    const totalNotes = useMemo(()=>{
+        return notes.length;
+    },[notes])
   return (
     <div>
-      
+      <h3>Total Notes: {totalNotes}</h3>
     </div>
   )
 }
