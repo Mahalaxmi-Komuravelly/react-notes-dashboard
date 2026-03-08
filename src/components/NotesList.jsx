64e49b3a53c1bf3e8f0404f6ce3df803634f37bf
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2026-03-08 17:12:16 +0530

    feat: add highlight note on click

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNotes } from '../context/NotesContext';
 const NotesList = () => {
     const {notes} = useNotes();
+    const [selectedIndex,setSelectedIndex] = useState(null);
 
   return (
     <div>
       <ul>
         {notes.map((note,i)=>(
-            <li key={i}>
+            <li key={i}
+            onClick={()=>setSelectedIndex(i)}
+            style={{
+                backgroundColor:selectedIndex === i ? "skyblue":"white",
+                cursor:"pointer",
+                margin:"5px",
+                padding:"5px",
+                border:"1px solid black",
+                listStyle:'none',
+            }}>
                 {note}
             </li>
 
